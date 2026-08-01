f991972b37cd70f4edbb46c9648f82c49040029f
Dhananjay Singh <172045152+dha-aa@users.noreply.github.com>
2026-08-01 21:03:04 +0530

    re write better

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 import express from "express";
 import {fileURLToPath} from "url";
 import path from "path";
+
+// In built modile
 import { getLocalIP } from "./utils/ip.js";
 import getQrcode from "./utils/qr.js";
 import getFiles from "./utils/download.js";
 import { upload } from "./upload/storage.js";
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
-
 const app = express();
 const PORT = 3000;
 
+// server the public dir
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/upload",upload,(req,res) => {
     console.log("uploaded!")
 })
 
-const url = `http://${getLocalIP()}:${PORT}`
-
 app.get("/api/files",(req,res) => {
     const files = getFiles()
     res.send(files)
 })
 
+// local url
+const url = `http://${getLocalIP()}:${PORT}`
 app.listen(PORT,(req,res) => {
     getQrcode(url);
     console.log(`Server on ${url}`);
