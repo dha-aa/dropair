e1c6cc6f280a868aec7542fee33dd507b79dbd67
Dhananjay Singh <172045152+dha-aa@users.noreply.github.com>
2026-08-02 09:26:30 +0530

    global install function now you can acces just by type in terminal:dropair

--- server.js
+++ server.js
@@ -9,39 +9,42 @@ import { upload } from "./upload/storage.js";
 import getCwd from "./utils/cwd.js";
 import getFiles from "./download/download.js";
 
-const app = express();
-const PORT = 3000;
+export default function startServer() {
+    const app = express();
+    const PORT = 3000;
 
-// server the public dir
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
-app.use(express.static(path.join(__dirname, "public")));
+    // server the public dir
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename)
+    app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/upload",upload,(req,res) => {
-    res.json({
-        msg:"uploaded succesfully"
+    app.post("/upload",upload,(req,res) => {
+        res.json({
+            msg:"uploaded succesfully"
+        })
     })
-})
 
-app.get("/api/files",(req,res) => {
-    const files = getFiles()
-    res.send(files)
-})
-
-const folderpath = getCwd();
-
-app.get("/download/:filename",(req,res) => {
-    const filename = req.params.filename;
-    const filepath = path.join(folderpath,filename);
-
-    res.download(filepath,filename ,(err) => {
-        console.log(err)
+    app.get("/api/files",(req,res) => {
+        const files = getFiles()
+        res.send(files)
     })
-})
 
-// local url
-const url = `http://${getLocalIP()}:${PORT}`
-app.listen(PORT,(req,res) => {
-    getQrcode(url);
-    console.log(`Server on ${url}`);
-})
+    const folderpath = getCwd();
+
+    app.get("/download/:filename",(req,res) => {
+        const filename = req.params.filename;
+        const filepath = path.join(folderpath,filename);
+
+        res.download(filepath,filename ,(err) => {
+            console.log(err)
+        })
+    })
+
+    // local url
+    const url = `http://${getLocalIP()}:${PORT}`
+    app.listen(PORT,(req,res) => {
+        getQrcode(url);
+        console.log(`Server on ${url}`);
+    });
+
+}
