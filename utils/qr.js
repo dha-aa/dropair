d6b7e663b000db2299a2415ced90c6d390bb1725
dha-aa <172045152+dha-aa@users.noreply.github.com>
2026-09-03 13:43:31 +0530

    writting the commnet

--- utils/qr.js
+++ utils/qr.js
@@ -1,5 +1,6 @@
 import qrcode from "qrcode-terminal";
 
+// Generates and prints a small QR code for the provided data in the terminal.
 export default function getQrcode(data) {
     qrcode.generate(data,{small:true})
 }
