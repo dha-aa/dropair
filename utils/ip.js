f5ed1a5a368df19a1ac4ec1c345df9eca09fe6d2
dha-aa <172045152+dha-aa@users.noreply.github.com>
2026-09-03 13:42:41 +0530

    writting the commnet

--- utils/ip.js
+++ utils/ip.js
@@ -1,5 +1,7 @@
 import os from "os";
 
+// Returns the first non-internal IPv4 address of the local machine.
+// Falls back to localhost if no suitable network interface is found.
 export function getLocalIP() {
     for (const networks of Object.values(os.networkInterfaces())) {
         for (const network of networks ?? []) {
