a8bc87cd317726ae4da0ce34a7269faaab70b71e
dha-aa <172045152+dha-aa@users.noreply.github.com>
2026-09-03 14:12:26 +0530

    refelect: code

--- download/download.js
+++ download/download.js
@@ -1,28 +1,12 @@
 import fs from "fs";
 import getCwd from "../utils/cwd.js";
 
-// Retrieves a list of files and folders in the current working directory, excluding hidden files (those starting with a dot).
 export default function  getFiles() {
     const cwd = getCwd()
-    const files = fs.readdirSync(cwd);
-    const listOfThing = []
-    for (const file of files) {
-
-        if (file.startsWith(".")) {
-            continue
-        }
-        //
-        const fullpath = `${cwd}/${file}`;
-        const statas = fs.statSync(fullpath);
-
-       
-        listOfThing.push({
-            filename: file,
-            type: statas.isDirectory() ? "folder":"file"
-        })
-    }
-    // Returns the list of files and folders in the current working directory.
-    // Example return value: [{filename: "example.txt", type: "file"}, {filename: "myFolder", type: "folder"}]
-    return listOfThing;
-    
+    return fs.readdirSync(cwd,{withFileTypes: true})
+        .filter(item => !item.name.startsWith("."))
+        .map(item => ({
+            filename:item.name,
+            type:item.isDirectory? "file" : "folder"
+        }))
 }
