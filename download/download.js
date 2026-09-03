c584313924e635c5b22d279adfc7f61728cef6a2
dha-aa <172045152+dha-aa@users.noreply.github.com>
2026-09-03 14:13:39 +0530

    comment

--- download/download.js
+++ download/download.js
@@ -1,8 +1,21 @@
 import fs from "fs";
 import getCwd from "../utils/cwd.js";
 
+// Returns an array of file objects in the current working directory, excluding hidden files (those starting with a dot). Each object contains the filename and its type (file or folder).  
 export default function  getFiles() {
     const cwd = getCwd()
+    
+    // exmple of the return value
+    // [
+    //     {
+    //         filename:"file1.txt",
+    //         type:"file"
+    //     },
+    //     {
+    //         filename:"folder1",
+    //         type:"folder"
+    //     }
+    // ]
     return fs.readdirSync(cwd,{withFileTypes: true})
         .filter(item => !item.name.startsWith("."))
         .map(item => ({
